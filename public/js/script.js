a94e19c9650de47bf27b2d630b8851183bad6171
Prima Aulia Gusta <primaulia@gmail.com>
2017-04-12 18:15:29 +0800

    latest css buggy

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,25 @@
 $(function () {
   var url = 'https://api.themoviedb.org/3/movie/popular?api_key=83ebcfbe2592e4358658da3522dad3ff'
 
+  var basePosterUrl = 'http://image.tmdb.org/t/p/w185'
+
   $.get(url, function (data) {
     console.log(data.results)
     var results = data.results
 
     results.forEach(function (movie) {
-      var $div = $('<div>')
+      var $div = $('<div class="four columns">')
       var $overview = $('<p>')
       $overview.text(movie.overview)
 
       $div
       .append('<h2>' + movie.title + '</h2>')
       .append($overview)
+      .append('<img src="' + basePosterUrl + movie.poster_path + '"/>')
+      .append('<br>')
       .append('<button>Add Review</button>')
 
-      $('.movie-list').append($div)
+      $('.row').append($div)
     })
   })
 })
