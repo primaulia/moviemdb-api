9448dc712eba5ed04e26cfc07f5f9b221355ab69
Prima Aulia Gusta <primaulia@gmail.com>
2017-04-13 10:00:39 +0800

    latest codes with api call

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// for calling external api
+var unirest = require('unirest')
+
+// express starts here
 var express = require('express')
 var app = express()
 
@@ -7,6 +11,35 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/yoda', (req, res) => {
+  // 1. get a random famous quotes X
+  // 2. get the output from the quotes api X
+  // 3. pass the data to yoda api
+  // 4. print the results out
+
+  // These code snippets use an open-source library. http://unirest.io/nodejs
+  unirest.post('https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous')
+  .header('X-Mashape-Key', 'y2WFWXi1A1mshLXvjdIjez35utEGp1FW9JgjsnMjbWIdC6nINL')
+  .header('Content-Type', 'application/x-www-form-urlencoded')
+  .header('Accept', 'application/json')
+  .end(function (output) {
+    var jsonOutput = JSON.parse(output.body)
+    var quote = jsonOutput.quote
+    unirest.get('https://yoda.p.mashape.com/yoda?sentence=' + quote)
+    .header('X-Mashape-Key', 'y2WFWXi1A1mshLXvjdIjez35utEGp1FW9JgjsnMjbWIdC6nINL')
+    .header('Accept', 'text/plain')
+    .end(function (output) {
+      console.log(quote)
+      res.render('yoda', {
+        quote: quote,
+        yoda: output.body
+      })
+    })
+  })
+
+  // These code snippets use an open-source library.
 })
 
 var port = process.env.PORT || 4000
